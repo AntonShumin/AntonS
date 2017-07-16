d28819d65c3f013bf5b3afcc1ef5e22869048299
AntonShumin <AntonShuminBe@gmail.com>
2017-07-16 23:21:37 +0200

    preset index page

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+//nothing here yet
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>Anton Shumin - Portfolio website</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Persoonlijke website van S.Anton" />
+    <meta name="author" content="Anton Shumin">
+    <meta charset="UTF-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+    <link rel="icon" type="image/ico" href="inc/images/icon.ico" />
+    <link href="inc/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="inc/css/style.css" rel="stylesheet" />
+    <link rel="stylesheet" href="inc/css/prettyPhoto.css" type="text/css" media="screen" />
+    <link href="inc/css/jquery.bxslider.min.css" rel="stylesheet" />
+    <link href="inc/css/font-awesome.min.css" rel="stylesheet" />
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css' />
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+
+
+</head>
+
+</html>

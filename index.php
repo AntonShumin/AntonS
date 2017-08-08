f7b83ba33c720a3c58ce49514ec837740feb7939
AntonShumin <AntonShuminBe@gmail.com>
2017-08-08 15:43:27 +0200

    include all template scripts

--- index.php
+++ index.php
@@ -44,9 +44,8 @@
                         <div class="slide-caption">
                             <div class="vertical-align-middle">
 
-                                <h1 class="ultralarge">Welcome</h1>
-                                <p>We are a creative agency. We build awesome things. We are crazy coders and creative designers.</p>
-                                <p><a href="http://themeforest.net/user/Hector18/portfolio" target="_blank" class="newave-button medium grey">Purchase now</a> &nbsp;&nbsp;&nbsp; <a href="#we-are-newave" class="newave-button medium outline white move">Take the tour</a></p>
+                                <h1 class="ultralarge">Hallo</h1>
+                                <p>Ik zie dat jij mijn pagina gevonden hebt, welkom!</p>
 
                             </div>
                         </div>
@@ -94,6 +93,57 @@
         </div>
     </div>
     <!--/Navigation -->
+
+
+
+    <!-- Footer -->
+    <footer>
+        <div class="container no-padding">
+
+            <a id="back-top"><div id="menu_top"><div id="menu_top_inside"></div></div></a>
+
+            <ul class="socials-icons">
+                <li><a href="#"><img src="inc/images/facebook.png" alt="" /></a></li>
+                <li><a href="#"><img src="inc/images/facebook.png" alt="" /></a></li>
+                <li><a href="#"><img src="inc/images/facebook.png" alt="" /></a></li>
+            </ul>
+
+            <p class="copyright">2017 &copy; Anton Shumin. All rights reserved.</p>
+
+        </div>
+    </footer>
+    <!--/Footer -->
+
+
+    <!--
+    <div style="opacity:0; height:0px!important; position:fixed; ">
+        <img  src="images/projects/3.png" alt="" />
+        <img  src="images/projects/4.jpg" alt="" />
+        <img  src="images/projects/5.jpg" alt="" />
+        <img  src="images/projects/6.png" alt="" />
+    </div>
+    -->
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="inc/js/jquery.sticky.js" type="text/javascript"></script>
+    <script src="inc/js/jquery.easing-1.3.pack.js" type="text/javascript"></script>
+    <script src="https://cdn.jsdelivr.net/tether/1.4.0/tether.min.js"></script>
+    <script src="inc/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="inc/js/jquery.parallax-1.1.3.js" type="text/javascript"></script>
+    <script src="inc/js/appear.js" type="text/javascript"></script>
+    <script src="inc/js/modernizr.js" type="text/javascript"></script>
+    <script src="inc/js/jquery.prettyPhoto.js" type="text/javascript"></script>
+    <script src="inc/js/isotope.pkgd.min.js" type="text/javascript"></script>
+    <script src="inc/js/jquery.bxslider.min.js" type="text/javascript"></script>
+    <script src="inc/js/jquery.cycle.all.js" type="text/javascript"></script>
+    <script src="inc/js/jquery.maximage.js" type="text/javascript"></script>
+    <script src="inc/js/sscr.js" type="text/javascript"></script>
+    <script src="inc/js/skrollr.min.js" type="text/javascript"></script>
+    <script src="inc/js/jquery.jigowatt.js" type="text/javascript"></script>
+    <script src="https://unpkg.com/imagesloaded@4/imagesloaded.pkgd.min.js"></script>
+
+    <script src="inc/js/scripts.js" type="text/javascript"></script>
 
 
 </body>
